35c5bea5894883733f64d79b37557c1c73920a11
Ricardo Landeros <rlande17@wgu.edu>
2026-04-03 00:10:02 -0700

    Add redirect from vercel.app domain to callsesh.com

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.238'],
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'callsesh.vercel.app',
+          },
+        ],
+        destination: 'https://callsesh.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
